87d34b800384fb93251787b286fd92010681064a
Santosh Pathak <santoshpathak.sp90@gmail.com>
2025-09-21 23:00:09 +0530

    initial commit

--- src/main/java/com/example/tele/LoginApp/repo/UserRepo.java
+++ src/main/java/com/example/tele/LoginApp/repo/UserRepo.java
@@ -2,10 +2,7 @@ package com.example.tele.LoginApp.repo;
 
 import com.example.tele.LoginApp.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
